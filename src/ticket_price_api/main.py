a2272fcb11b170445bd657e5027fdc5ebcd01a50
Vladimir Klepov <v.klepov@gmail.com>
2024-10-01 17:18:31 +0300

    Load model from repository (#3)
    * Load model from repository
    
    * Add MLFLOW tracking URI to deployment

--- src/ticket_price_api/main.py
+++ src/ticket_price_api/main.py
@@ -1,13 +1,11 @@
 """FastAPI router module."""
 
 from fastapi import FastAPI
-
-from . import schemas
+from . import schemas, model
 
 app = FastAPI()
 
 
 @app.post("/predict", response_model=list[schemas.PricePrediction])
 async def get_matching_jobs(flights: list[schemas.Flight]):
-    prediction = [schemas.PricePrediction(flight=flight, price=0) for flight in flights]
-    return prediction
+    return model.predict(flights)
